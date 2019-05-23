ae205941637d5ed093a26c3a56017c4399d611d1
ArtificialNerd <eruiz42100@gmail.com>
2019-05-23 20:44:07 -0300

    Auto fix
    Terminada la clase de Auto con la implementación de MiCalendario

--- Auto/src/Auto/Auto.java
+++ Auto/src/Auto/Auto.java
@@ -5,87 +5,46 @@
  */
 package Auto;
 
-import java.util.Calendar;
-
 /**
  *
  * @author Rodrigo
  */
 public class Auto extends Motorizado{
     
-     public Auto(){
+    private String marca;
+    private String modelo;
+
+    public Auto(){
         super();
     }
-
-    public Auto(String vin, Calendar fechaFab) throws MotorizadoExeption {
+    public Auto(String vin, MiCalendario fechaFab) throws MotorizadoExeption {
         super(vin, fechaFab);
-        
     }
-
     
+    public Auto(String vin, MiCalendario fechaFab, String marca) throws MotorizadoExeption {
+        super(vin, fechaFab);
+        this.marca = marca;
+    }
     
-    private String marca;
-    
-    private String modelo;
-    
-    private String patente;
-    
-    private Calendar fechaDePatentamiento;
-    
-    private Double precio;
-    
-    private Integer anio;
-
-    
-
+    public Auto(String vin, MiCalendario fechaFab, String marca, String modelo) throws MotorizadoExeption {
+        super(vin, fechaFab);
+        this.marca = marca;
+        this.modelo = modelo;
+    }
 
     public String getMarca() {
         return marca;
     }
 
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
     public String getModelo() {
         return modelo;
+    }
+    
+    public void setMarca(String marca) {
+        this.marca = marca;
     }
 
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
-
-    public String getPatente() {
-        return patente;
-    }
-
-    public void setPatente(String patente) {
-        this.patente = patente;
-    }
-
-    public Calendar getFechaDePatentamiento() {
-        return fechaDePatentamiento;
-    }
-
-    public void setFechaDePatentamiento(Calendar fechaDePatentamiento) {
-        this.fechaDePatentamiento = fechaDePatentamiento;
-    }
-
-    public Double getPrecio() {
-        return precio;
-    }
-
-    public void setPrecio(Double precio) {
-        this.precio = precio;
-    }
-
-    public Integer getAnio() {
-        return anio;
-    }
-
-    public void setAnio(Integer anio) {
-        this.anio = anio;
-    }
-    
-   
 }
